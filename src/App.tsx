8020da79d5af2909da0326038e1c9d43c8e3b126
uoshvis <uoshvis@gmail.com>
2023-02-22 19:06:15 +0200

    add interface Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Props {
+    url: string
+    children: React.ReactNode
 }
 
-export default App;
+const Link = ({ url, children }: Props) => {
+    return (
+        <a
+            className="App-link"
+            target="_blank"
+            rel="noopener noreferrer"
+            href={url}
+        >
+            {children}
+        </a>
+    )
+}
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <Link url="https://reactjs.org">
+                    Learn React with TypeScript
+                </Link>
+            </header>
+        </div>
+    )
+}
+
+export default App
